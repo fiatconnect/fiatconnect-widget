8cc8ed727bd186a9c6f88fb8db584c21fdb35c78
Charlie Andrews-Jubelt <cajubelt@users.noreply.github.com>
2023-10-07 15:58:38 -0700

    ci: linting, prettier, depcheck, add github actions (#2)
    * linting and gha
    
    * depcheck

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,26 @@
-import React from 'react';
-import './App.css';
-import '@rainbow-me/rainbowkit/styles.css';
+import React from 'react'
+import './App.css'
+import '@rainbow-me/rainbowkit/styles.css'
 
+import { publicProvider } from 'wagmi/providers/public'
+import { configureChains, createConfig, WagmiConfig } from 'wagmi'
+import { celo } from 'viem/chains'
+import {
+  ConnectButton,
+  getDefaultWallets,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit'
 
-import {publicProvider} from 'wagmi/providers/public';
-import {configureChains, createConfig, WagmiConfig} from 'wagmi';
-import {celo} from 'viem/chains';
-import {ConnectButton, getDefaultWallets, RainbowKitProvider} from '@rainbow-me/rainbowkit';
-
-const {chains, publicClient} = configureChains(
-  [celo],
-  [
-    publicProvider()
-  ]
-);
-const {connectors} = getDefaultWallets({
+const { chains, publicClient } = configureChains([celo], [publicProvider()])
+const { connectors } = getDefaultWallets({
   appName: 'My RainbowKit App',
   projectId: 'ccf8cc7da29e8b1ed52a455b808f2699',
-  chains
-});
+  chains,
+})
 const wagmiConfig = createConfig({
   autoConnect: true,
   connectors,
-  publicClient
+  publicClient,
 })
 
 function App() {
@@ -32,14 +30,12 @@ function App() {
         <div className="App">
           <header className="App-header">
             <ConnectButton />
-            <p>
-              Hello, world!
-            </p>
+            <p>Hello, world!</p>
           </header>
         </div>
       </RainbowKitProvider>
     </WagmiConfig>
-  );
+  )
 }
 
-export default App;
+export default App
