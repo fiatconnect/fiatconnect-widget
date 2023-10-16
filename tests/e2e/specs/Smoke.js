8eea5f7da051f47010319642a70f1f11854c928c
MuckT <mcgtom10@gmail.com>
2023-10-15 17:12:38 -0700

    test(e2e): add Synpress e2e framework and sample tests

---
+++ tests/e2e/specs/Smoke.js
@@ -0,0 +1,16 @@
+
+describe('Smoke Test', () => {
+  it('Able to connect via MetaMask', () => {
+    cy.visit('')
+    cy.get('[data-testid="rk-account-button"]').should('not.exist')
+    cy.get('[data-testid="rk-connect-button"]').click()
+    cy.get('[data-testid="rk-wallet-option-metaMask"]').click()
+    cy.acceptMetamaskAccess()
+    cy.get('[data-testid="rk-account-button"]').should('be.visible')
+  })
+
+  it('Able to view connected wallet address', () => {
+    cy.get('[data-testid="rk-account-button"]').click()
+    cy.get('#rk_profile_title').should('have.text', '0x64…09b9')
+  })
+})
