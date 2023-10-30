b1478b54dab12aee1e16c9f05752c1f63aa12605
Charlie Andrews <charlie.andrews@valoraapp.com>
2023-10-29 19:21:34 -0500

    use router and useSearchParams hook instead of window.location to get search params

--- src/hooks.ts
+++ src/hooks.ts
@@ -2,12 +2,12 @@ import { FiatConnectClientConfig } from '@fiatconnect/fiatconnect-sdk'
 import { providerIdToBaseUrl } from './FiatConnectClient'
 import { useAccount, useNetwork } from 'wagmi'
 import { chainIdToFiatConnectNetwork } from './constants'
+import { useSearchParams } from 'react-router-dom'
 
 export function useFiatConnectConfig(): FiatConnectClientConfig | undefined {
   const account = useAccount()
   const network = useNetwork()
-  // TODO: The "right" way to do this is probably through react-router-dom's "useSearchParams" hook
-  const searchParams = new URLSearchParams(window.location.search)
+  const [searchParams] = useSearchParams()
   const apiKey = searchParams.get('apiKey')
   const providerId = searchParams.get('providerId') ?? undefined
 
