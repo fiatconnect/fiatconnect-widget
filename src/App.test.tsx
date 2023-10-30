c278882cd210b9ed705fa8232b8080582ea17c80
Charlie Andrews <charlie.andrews@valoraapp.com>
2023-10-29 19:44:56 -0500

    mock router

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { render, screen } from '@testing-library/react'
 import App from './App'
 
+jest.mock('react-router-dom')
 describe('App', () => {
   it('renders wallet connect element', () => {
     render(<App />)
