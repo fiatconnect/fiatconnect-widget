c236af416387db36fc2796c2c56ad6125119904b
MuckT <mcgtom10@gmail.com>
2023-11-14 00:56:45 -0800

    refactor: use styled-components for steps header

--- src/components/StepsHeader.tsx
+++ src/components/StepsHeader.tsx
@@ -1,5 +1,6 @@
 import { Steps } from '../types'
 import { TransferType } from '@fiatconnect/fiatconnect-types'
+import styled from 'styled-components'
 
 interface Props {
   step: Steps
@@ -24,32 +25,98 @@ export function StepsHeader({
   const makeSections = () => {
     const sections = []
     for (let curStep = 1; curStep <= MAX_STEPS; curStep++) {
-      const id =
+      const Component =
         step === curStep
-          ? 'StepsCircle-Current'
+          ? StepsCircleCurrent
           : step > curStep
-          ? 'StepsCircle-Complete'
-          : 'StepsCircle-Inactive'
+          ? StepsCircleComplete
+          : StepsCircleInactive
       sections.push(
-        <div key={`StepsCircle-${curStep}`} id={id}>
+        <Component key={`StepsCircle-${curStep}`}>
           <div>{curStep}</div>
-          <div id="StepsHeader-Title">{STEP_TO_TITLE[curStep]}</div>
-        </div>,
+          <StepsHeaderTitle>{STEP_TO_TITLE[curStep]}</StepsHeaderTitle>
+        </Component>,
       )
       if (curStep !== MAX_STEPS) {
         sections.push(
-          <div
-            id="StepsCircle-Divider"
-            key={`StepsCircle-Divider-${curStep}`}
-          />,
+          <StepsCircleDivider key={`StepsCircle-Divider-${curStep}`} />,
         )
       }
     }
     return sections
   }
   return (
-    <div className="StepsHeader">
-      <div className="StepsHeader-Progress">{makeSections()}</div>
-    </div>
+    <Container>
+      <StepsHeaderProgress>{makeSections()}</StepsHeaderProgress>
+    </Container>
   )
 }
+
+const Container = styled.div`
+  padding: 10px;
+  padding-bottom: 40px;
+  display: flex;
+  flex-direction: column;
+  font-weight: 200;
+  font-size: 11px;
+  line-height: 17px;
+`
+
+const StepsCircleComplete = styled.div`
+  position: relative;
+  display: flex;
+  height: 18px;
+  width: 18px;
+  border-radius: 50%;
+  border: 1px solid #bacdff;
+  color: #bacdff;
+  display: flex;
+  justify-content: center;
+`
+
+const StepsCircleCurrent = styled.div`
+  position: relative;
+  height: 18px;
+  width: 18px;
+  border-radius: 50%;
+  border: 1px solid #5987ff;
+  color: #5987ff;
+  display: flex;
+  justify-content: center;
+`
+
+const StepsCircleInactive = styled.div`
+  position: relative;
+  height: 18px;
+  width: 18px;
+  border-radius: 50%;
+  border: 1px solid #868686;
+  color: #868686;
+  display: flex;
+  justify-content: center;
+`
+
+const StepsHeaderProgress = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`
+
+const StepsCircleDivider = styled.div`
+  align-self: center;
+  display: flex;
+  height: 0px;
+  width: 27%;
+  border: 1px solid #cbcbcb;
+`
+
+const StepsHeaderTitle = styled.div`
+  position: absolute;
+  display: table;
+  top: 100%;
+  white-space: nowrap;
+  text-align: center;
+  font-size: 10px;
+  justify-content: space-between;
+  color: black;
+`
