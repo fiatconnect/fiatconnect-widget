370b424d9fbec1a22e87631ee358fe2234a06b87
Charlie Andrews <charlie.andrews@valoraapp.com>
2023-11-13 21:43:06 -0800

    fix jest errors

--- jest.config.js
+++ jest.config.js
@@ -19,6 +19,9 @@ module.exports = {
     },
   },
   moduleNameMapper: {
+    '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      'identity-obj-proxy',
     '\\.(css|less)$': 'identity-obj-proxy',
+    '@fontsource/inter': 'identity-obj-proxy',
   },
 }
