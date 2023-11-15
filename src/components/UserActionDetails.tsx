4173692d0abe46ad3f0c95c0fd43cdd3d61fd998
Charlie Andrews <charlie.andrews@valoraapp.com>
2023-11-14 19:28:31 -0800

    WIP add styles.ts and UserActionDetails.tsx

---
+++ src/components/UserActionDetails.tsx
@@ -0,0 +1,5 @@
+import { contentContainerStyle } from '../styles'
+
+export function UserActionDetails() {
+  return <div style={contentContainerStyle}>User action details placeholder</div>
+}
---
+++ src/styles.ts
@@ -0,0 +1,7 @@
+export const contentContainerStyle = {
+  height: '100%',
+  width: '100%',
+  display: 'flex',
+  'flex-direction': 'column',
+  'align-items': 'flex-start',
+}
