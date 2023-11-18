292742abe1baf32f2b249707f99da45ab7498cfd
Charlie Andrews <charlie.andrews@valoraapp.com>
2023-11-17 23:02:59 -0800

    prettier

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
-import {createHashRouter, RouterProvider} from 'react-router-dom'
+import { createHashRouter, RouterProvider } from 'react-router-dom'
 
 const router = createHashRouter([
   {
