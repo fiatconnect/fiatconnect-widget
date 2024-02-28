5bbf4a6ef39dfdb33a852c6f36631b830efe5984
Charlie Andrews <charlie.andrews@valoraapp.com>
2024-02-28 14:34:01 -0800

    add test coverage

--- src/components/KYCInfoFieldSection.test.tsx
+++ src/components/KYCInfoFieldSection.test.tsx
@@ -1,4 +1,5 @@
 import { getDropdownValues } from './KYCInfoFieldSection'
+import {idTypeReverseFormatter} from "./kycInfo/PersonalDataAndDocumentsDetailed";
 
 describe('KYCInfoFieldSection', () => {
   describe('getDropdownValues', () => {
@@ -6,36 +7,80 @@ describe('KYCInfoFieldSection', () => {
       const allowedValues: [string, ...string[]] = ['a', 'b']
       const choices = undefined
       const reverseFormatter = undefined
-      const result = getDropdownValues({ allowedValues, choices, reverseFormatter })
+      const result = getDropdownValues({
+        allowedValues,
+        choices,
+        reverseFormatter,
+      })
       expect(result).toEqual(allowedValues)
     })
     it('returns choices if allowedValues is undefined', () => {
       const allowedValues = undefined
       const choices: [string, ...string[]] = ['a', 'b']
       const reverseFormatter = undefined
-      const result = getDropdownValues({ allowedValues, choices, reverseFormatter })
+      const result = getDropdownValues({
+        allowedValues,
+        choices,
+        reverseFormatter,
+      })
       expect(result).toEqual(choices)
     })
     it('returns choices that are in allowedValues', () => {
       const allowedValues: [string, ...string[]] = ['a', 'b']
       const choices: [string, ...string[]] = ['a', 'b', 'c']
       const reverseFormatter = undefined
-      const result = getDropdownValues({ allowedValues, choices, reverseFormatter })
+      const result = getDropdownValues({
+        allowedValues,
+        choices,
+        reverseFormatter,
+      })
       expect(result).toEqual(allowedValues)
     })
     it('returns allowedValues if no choices are in allowedValues', () => {
       const allowedValues: [string, ...string[]] = ['a', 'b']
       const choices: [string, ...string[]] = ['c', 'd']
       const reverseFormatter = undefined
-      const result = getDropdownValues({ allowedValues, choices, reverseFormatter })
+      const result = getDropdownValues({
+        allowedValues,
+        choices,
+        reverseFormatter,
+      })
       expect(result).toEqual(allowedValues)
     })
-    it('returns formatted values', () => {
-      const allowedValues: [string, ...string[]] = ['a', 'b']
-      const choices: [string, ...string[]] = ['A', 'B', 'C']
-      const reverseFormatter = (x: string) => x.toUpperCase()
-      const result = getDropdownValues({ allowedValues, choices, reverseFormatter })
-      expect(result).toEqual(['A', 'B'])
+    it('returns human-readable values', () => {
+      const optionAid = 'a'
+      const optionAhumanReadable = 'Option A'
+      const optionBid = 'b'
+      const optionBhumanReadable = 'Option B'
+      const allowedValues: [string, ...string[]] = [optionAid, optionBid]
+      const choices: [string, ...string[]] = [
+        optionAhumanReadable,
+        optionBhumanReadable,
+        'Option C',
+      ]
+      const reverseFormatter = (optionId: string) =>
+        `Option ${optionId.toUpperCase()}`
+      const result = getDropdownValues({
+        allowedValues,
+        choices,
+        reverseFormatter,
+      })
+      expect(result).toEqual(['Option A', 'Option B'])
+    })
+    it('works for ID type field', () => {
+      const allowedValues: [string, ...string[]] = ['IDC', 'PAS']
+      const choices: [string, ...string[]] = [
+        'State-issued ID',
+        'Passport',
+        "Driver's License",
+      ]
+      const reverseFormatter = idTypeReverseFormatter
+      const result = getDropdownValues({
+        allowedValues,
+        choices,
+        reverseFormatter,
+      })
+      expect(result).toEqual(['State-issued ID', 'Passport'])
     })
   })
 })
